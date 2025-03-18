202670746b7565964cacd89fa5e2015a89190d5f
Chu-qiulan <1606464637@qq.com>
2025-03-18 19:38:38 +0800

    Add files via upload

---
+++ addtwonums2.py
@@ -0,0 +1,9 @@
+# Add Two Numbers in Python
+# Author: Your Name here
+# Using user input
+# taking user input
+a = input("First number: ")
+b = input("Second number: ")
+# converting input to float and adding
+res = float(a) + float(b)
+print(res)
