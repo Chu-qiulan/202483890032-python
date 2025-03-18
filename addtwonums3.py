eb036250e13cfe3bfd42c1a4b9c32a513c4d6f67
Chu-qiulan <1606464637@qq.com>
2025-03-18 19:39:30 +0800

    Add files via upload

---
+++ addtwonums3.py
@@ -0,0 +1,16 @@
+# Add Two Numbers in Python
+# Author: Your Name here
+# Using a function
+# function to add two numbers
+def add(a,b):
+ #converting input to float and adding
+ result = float(a) + float(b)
+ return result
+# taking user input
+a = input("First Number: ")
+b = input("Second Number: ")
+# calling function
+res = add(a,b)
+print("The Answer is:")
+print(res)
+
